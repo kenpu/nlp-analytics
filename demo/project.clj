6228e4b9856c442ae68a3c2f61dd5db1f173b645
Blair Wiser <blair.m.wiser@gmail.com>
2015-06-09 13:10:02 -0400

    Created score-sentence route to handler.clj. Added necessary dependencies to project.clj.

--- demo/project.clj
+++ demo/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.3.1"]
                  [clojure-opennlp "0.3.3"]
                  [berkeleylm/berkeleylm "1.1.5"]]
   :plugins [[lein-ring "0.8.13"]]
--- demo/src/demo/handler.clj
+++ demo/src/demo/handler.clj
@@ -1,11 +1,35 @@
 (ns demo.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.handler :as handler]
+            [ring.middleware.json :as middleware]
+            [ring.util.response :as resp])
+  (:use demo.scoring))
+
+(defn determine-method
+  [method sentence]
+  (condp = method
+    "pos-maxent" (score-by-pos-maxent sentence)
+    "pos-perceptron" (score-by-pos-perceptron sentence)
+    "blm" (score-by-blm sentence)
+    (str "Invalid method selection")))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (POST "/score-sentence/" request
+        (let [sentence (:sentence request)
+              method (:method request)
+              includeSpanScores (:includeSpanScores request)
+              spanNgram (:spanNgram request)
+              result {
+                      :words (tokenize (str sentence))
+                      :sentenceScore (determine-method method (str sentence))
+                      }]
+          (if (true? includeSpanScores)
+            (assoc result (score-spans-by-blm (str sentence) spanNgram))
+            result)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+(-> (handler/site app-routes)
+  (middleware/wrap-json-body {:keywords? true}) 
+  (middleware/wrap-json-response)))
