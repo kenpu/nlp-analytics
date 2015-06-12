c8e252f88aa64416ec6488ecabb421f0607a9bf5
Blair Wiser <blair.m.wiser@gmail.com>
2015-06-12 13:36:49 -0400

    Refactored handler.clj for organization and data handling.

--- demo/src/demo/handler.clj
+++ demo/src/demo/handler.clj
@@ -18,28 +18,17 @@
   [request name]
   (get-in request [:params name]))
 
-(defn get-span-scores
-  [includeSpanScores sentence spanNgram]
-  (if includeSpanScores
-    (vector (score-spans-by-blm sentence spanNgram))
-    [])
-  )
+(def goog-lm (make-google-lm-fake))
 
 (defn score-sentence
   [sentence method includeSpanScores spanNgram]
-  {:words (tokenize sentence)
-   :sentenceScore (determine-method method sentence)
-   :spanScores (get-span-scores includeSpanScores sentence spanNgram)})
-
-(defn score-text
-  [text method includeSpanScores spanNgram]
-  (loop [remain (get-sentences text) result []]
-    (if (empty? remain)
-      result
-      (recur (rest remain) (conj result {:sentence (first remain)
-                                         :words (tokenize (first remain))
-                                         :sentenceScore (determine-method method (first remain))
-                                         :spanScores (get-span-scores includeSpanScores (first remain) spanNgram)})))))
+  (let [words (tokenize sentence)
+        score (condp = method
+                "pos-maxent" (score-by-pos-maxent words includeSpanScores)
+                "pos-perceptron" (score-by-pos-perceptron words includeSpanScores)
+                "blm" (score-by-blm {:words words :N spanNgram :includeSpanScores includeSpanScores :lm goog-lm}))]
+    {:status 200
+     :body (assoc score :words words)}))
 
 (defroutes app-routes
   (POST "/score-sentence/" request
@@ -47,15 +36,16 @@
               method (param request :method)
               includeSpanScores (param request :includeSpanScores)
               spanNgram (Integer/parseInt (param request :spanNgram))]
-          {:status 200
-           :body (score-sentence sentence method includeSpanScores spanNgram)}))
+           (score-sentence sentence method includeSpanScores spanNgram)))
+  
   (POST "/score-text/" request
         (let [text (param request :text)
               method (param request :method)
               includeSpanScores (param request :includeSpanScores)
               spanNgram (Integer/parseInt (param request :spanNgram))]
           {:status 200
-           :body {:result (score-text text method includeSpanScores spanNgram)}}))
+           :body "Not implemented"}))
+  
   (route/not-found "Not Found"))
 
 (def app
--- demo/src/demo/scoring.clj
+++ demo/src/demo/scoring.clj
@@ -7,31 +7,64 @@
 (def pos-perceptron (make-pos-tagger "resources/models/en-pos-perceptron.bin"))
 (def get-sentences (make-sentence-detector "resources/models/en-sent.bin"))
 
-(def google-lm
-  (let [modeldir "/home/blair/NLP/models" ;;(System/getenv "LMDIR")
-        vocab (str modeldir "/vocab_cs.gz")
-        lm (str modeldir "/eng.blm")]
-    (if (empty? modeldir)
-      (throw (Exception. "Environment variable LMDIR is not set"))
-      (LmReaders/readGoogleLmBinary lm vocab))))
+(defn make-google-lm
+  []
+  (do
+    (println "Loading the Google language model.")
+    (let [modeldir "/home/blair/NLP/models" ;;(System/getenv "LMDIR")
+          vocab (str modeldir "/vocab_cs.gz")
+          lm (str modeldir "/eng.blm")
+          goog-lm (if (empty? modeldir)
+                    (throw (Exception. "Environment variable LMDIR is not set"))
+                    (LmReaders/readGoogleLmBinary lm vocab))]
+      (println "Completed")
+      goog-lm)))
+
+(defn make-google-lm-fake [] nil)
+
+(defn get-log-prob 
+  [n-gram lm]
+  (if (nil? lm)
+    -100
+    (.getLogProb lm n-gram)))
+
+(defn make-ngrams
+  "Returns a seq for each consecutive n words in words"
+  [n words]
+  (let [L (max 0 (- (count words) n))]
+    (for [i (range 0 (inc L))]
+     (subvec words i (min (count words) (+ i n))))))
+
+(defn make-score
+  "Prepares return data"
+  [total-prob & args]
+  (if (empty? args)
+    {:sentenceScore total-prob}
+    {:sentenceScore total-prob
+     :spanScores (first args)}))
 
 (defn score-by-pos-maxent
-  [s]
-  (apply * (:probabilities (meta (pos-maxent (tokenize s))))))
+  [words includeSpanScores]
+  (let [probs (map #(Math/log %) (:probabilities (meta (pos-maxent words))))
+        total-prob (apply + probs)]
+    (if includeSpanScores
+      (make-score total-prob probs)
+      (make-score total-prob))))
 
 (defn score-by-pos-perceptron
-  [s]
-  (apply * (:probabilities (meta (pos-perceptron (tokenize s))))))
+  [words includeSpanScores]
+  (let [probs (map #(Math/log %) (:probabilities (meta (pos-perceptron words))))
+        total-prob (apply + probs)]
+    (if includeSpanScores
+      (make-score total-prob probs)
+      (make-score total-prob))))
 
 (defn score-by-blm
-  [s]
-  (.getLogProb google-lm (tokenize s)))
-  
-  
-(defn score-spans-by-blm
-  [s n]
-  (loop [remain (tokenize s)
-         result []]
-    (if (empty? remain)
-      result
-      (recur (rest remain) (conj result (.getLogProb google-lm (take n remain)))))))
+  "s: sentence<string>, N:n-gram length<int>, includeSpanScores: <boolean>"
+  [{:keys [words N includeSpanScores lm]}]
+  (let [n-grams (make-ngrams N words)
+        probs (map #(get-log-prob % lm) n-grams)
+        total-prob (apply + probs)]
+    (if includeSpanScores
+      (make-score total-prob probs)
+      (make-score total-prob))))
