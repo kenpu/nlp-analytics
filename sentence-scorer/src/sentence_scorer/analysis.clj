066ac90c51263e3bf3c0ec436ea43afe1014e809
Blair Wiser <blair.m.wiser@gmail.com>
2015-07-27 13:48:42 -0400

    Added get-mean function to analysis.clj. Also added first version of get-sentence-score.

--- sentence-scorer/src/sentence_scorer/analysis.clj
+++ sentence-scorer/src/sentence_scorer/analysis.clj
@@ -1,9 +1,14 @@
 (ns sentence-scorer.analysis)
 
+(defn get-mean
+  "Takes a list and returns the mean"
+  [scores]
+  (/ (apply + scores) (count scores)))
+
 (defn get-standard-deviations
   "Takes list of scores and returns a corresponding list of how many standard deviations the value is within."
   [scores]
-  (let [mean (/ (apply + scores) (count scores))
+  (let [mean (get-mean scores)
         differences (map #(- % mean) scores)
         variance (/ (apply + (map #(Math/pow % 2) differences)) (count differences))
         standard-dev (Math/sqrt variance)]
@@ -15,3 +20,8 @@
   (let [threshold -1.5]
     {:num-dips (count (filter #(< % threshold) scores))
      :max-dip (apply min scores)}))
+
+(defn get-sentence-score
+  "Returns a sentence's overall score"
+  [scores]
+  (get-mean (map get-mean scores)))
