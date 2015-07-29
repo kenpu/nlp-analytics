5537f546d8a2adfbfcb6bb7ea2af4ecbc745a41e
Blair Wiser <blair.m.wiser@gmail.com>
2015-07-29 11:52:58 -0400

    Reorganized get-n-grams and get-sentence-vectors from core.clj to analysis.clj. get-n-grams now takes a language model as an argument.

--- sentence-scorer/src/sentence_scorer/analysis.clj
+++ sentence-scorer/src/sentence_scorer/analysis.clj
@@ -1,4 +1,5 @@
-(ns sentence-scorer.analysis)
+(ns sentence-scorer.analysis
+  (:use [sentence-scorer.evaluate]))
 
 (defn get-weighted-mean
   "Takes a list and returns the mean"
@@ -8,3 +9,16 @@
         terms (for [i (range k) :let [Wi (nth W i)]]
                 (* Wi (nth scores i)))]
     (/ (apply + terms) k)))
+
+(defn get-n-grams
+  "Takes a sentence and returns its n-gram scores for n = 5 through 1"
+  [lm sentence]
+  (let [words (tokenize sentence)]
+    (for [i (range 5 0 -1)]
+      (:spanScores (score-by-blm {:words words :N i :lm lm})))))
+
+(defn get-sentence-vectors
+  "Takes n-gram scores and returns aggregated relative scores"
+  [ngrams]
+  (for [i (range 0 5) :let [s (nth ngrams i)]]
+    (get-weighted-mean s)))
--- sentence-scorer/src/sentence_scorer/core.clj
+++ sentence-scorer/src/sentence_scorer/core.clj
@@ -4,23 +4,10 @@
 
 (def lm (make-google-lm-fake))
 
-(defn get-n-grams
-  "Takes a sentence and returns its n-gram scores for n = 5 through 1"
-  [sentence]
-  (let [words (tokenize sentence)]
-    (for [i (range 5 0 -1)]
-      (:spanScores (score-by-blm {:words words :N i :lm lm})))))
-
-(defn get-sentence-vectors
-  "Takes n-gram scores and returns aggregated relative scores"
-  [ngrams]
-  (for [i (range 0 5) :let [s (nth ngrams i)]]
-    (get-weighted-mean s)))
-
 (defn score-sentence
   "Takes a sentence and returns score vector"
   [sentence]
-  (get-sentence-vectors (get-n-grams sentence)))
+  (get-sentence-vectors (get-n-grams lm sentence)))
 
 (defn -main
   [& args]
