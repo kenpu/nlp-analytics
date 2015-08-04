83650a622d9355cb974341076a666c0ff9f80182
Blair Wiser <blair.m.wiser@gmail.com>
2015-08-04 13:50:44 -0400

    Changed to load real lm.

--- sentence-scorer/src/sentence_scorer/core.clj
+++ sentence-scorer/src/sentence_scorer/core.clj
@@ -3,7 +3,7 @@
         [sentence-scorer.analysis])
   (:require [clojure.string :as str]))
 
-(def lm (make-google-lm-fake))
+(def lm (make-google-lm))
 (def directory "resources/public/")
 
 (defn score-sentence
